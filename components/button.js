27092c213c17adc9964fd64d207146cf82d0accb
Steven Sosebee <swsosebee@gmail.com>
2023-09-12 22:52:33 -0400

    refactor button component

--- components/button.js
+++ components/button.js
@@ -1,13 +1,20 @@
 import React, { useState, useContext } from "react";
-import { useThemeContext } from "../../providers/theme/provider_theme";
-import { useModalContext } from "../../providers/modal/provider_modal";
 
-export const Button = ({ handleClick, styleClass, buttonText, buttonId, buttonIcon,buttonData }) => {
-  const { theme, setTheme } = useThemeContext();
-  const { modal, setModal } = useModalContext();
-  return (
-    <button data-id={buttonId} className={`${styleClass} ${theme}`} onClick={handleClick}>
-      {(buttonIcon)? (<i className={buttonIcon}></i>): (buttonText)}
-    </button>
-  );
-};
+export const Button = ({
+  buttonClick, 
+  buttonClass, 
+  buttonText, 
+  buttonId, 
+  buttonIcon, 
+  buttonData
+  }) => {
+    return (
+      <button
+        data-id={buttonId}
+        className={buttonClass}
+        onClick={buttonClick}
+        >
+          {(buttonIcon)? (<i className={buttonIcon}></i>): (buttonText)}
+      </button>
+      )
+}
